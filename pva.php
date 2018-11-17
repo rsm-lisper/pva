d9c0232602800bfebdb2e3ef19de08f0c92b3b45
Rafał -rsm- Marek <rsm@lisper.pl>
2018-11-17 23:20:55 +0100

    wstępne phpdoc i testy

--- pva.php
+++ pva.php
@@ -2,19 +2,37 @@
 
 namespace pva;
 
-function every (callable $predicate, mixed $arg0, ...$args) { return
+/**
+ * @param callable $predicate
+ * @param mixed $arg0
+ * @param mixed ...$args
+ * @return bool
+ */
+function every ($predicate, $arg0, ...$args) { return
         (count ($args) === 0 ? $predicate ($arg0) === true :
          ($predicate ($arg0) === true ? every ($predicate, array_shift ($args), ...$args) :
           false)) ;}
 
 
-function some (callable $predicate, mixed $arg0, ...$args) { return
+/**
+ * @param callable $predicate
+ * @param mixed $arg0
+ * @param mixed ...$args
+ * @return bool
+ */
+function some ($predicate, $arg0, ...$args) { return
         (count ($args) === 0 ? $predicate ($arg0) === true :
          ($predicate ($arg0) === true ? true :
           some ($predicate, array_shift ($args), ...$args))) ;}
 
 
-function none (callable $predicate, mixed $arg0, ...$args) { return
+/**
+ * @param callable $predicate
+ * @param mixed $arg0
+ * @param mixed ...$args
+ * @return bool
+ */
+function none ($predicate, $arg0, ...$args) { return
         (count ($args) === 0 ? $predicate ($arg0) === false :
          ($predicate ($arg0) === false ? none ($predicate, array_shift ($args), ...$args) :
           false)) ;}
